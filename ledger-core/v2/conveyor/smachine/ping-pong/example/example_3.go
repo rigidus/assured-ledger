e556afc237b3a3f867d0f138c75909216e696ea4
Rigidus <avenger-f@yandex.ru>
2020-01-27 11:12:06 +0300

    remove dbg printfs

--- ledger-core/v2/conveyor/smachine/ping-pong/example/example_3.go
+++ ledger-core/v2/conveyor/smachine/ping-pong/example/example_3.go
@@ -78,18 +78,14 @@ func (s *StateMachine3) Start(ctx smachine.ExecutionContext) smachine.StateUpdat
 		// Make-pair found
 		mySharedAccessReport := v.PrepareWithSecondPlayer(func(state *CustomSharedState) {
 			// First Player is already there, I will be Second Player
-			fmt.Printf("Start:%p (Shared), set Second = %p, Pair=%p \n", s, s, state.FirstPlayer)
 			// Key for Game Object
 			key := fmt.Sprintf("%p.%p", state.FirstPlayer, state.SecondPlayer)
-			fmt.Printf("key=%s \n", key)
 			s.waitKey = key //longbits.ByteString(key)
 		},s ).TryUse(ctx)
 		return smachine.RepeatOrJumpElse(ctx, mySharedAccessReport, s.WaitForGame, s.Wrong)
 	} else {
 		// Make-pair not found
 		report := s.catalogC.GetOrCreate(ctx, s.testKey).PrepareWithFirstPlayer(func(state *CustomSharedState) {
-			//state.FirstPlayer = s // I will be First Player
-			fmt.Printf("Start:%p (no Shared), set First = %p \n", s, s)
 		}, s).TryUse(ctx)
 		return smachine.RepeatOrJumpElse(ctx, report, s.WaitForSecond, s.Wrong)
 	}
@@ -103,9 +99,7 @@ func (s *StateMachine3) WaitForSecond(ctx smachine.ExecutionContext) smachine.St
 			if "" != state.SecondPlayer {
 				switcher = true // Second player is here!
 				s.waitKey = fmt.Sprintf("%p.%p", state.FirstPlayer, state.SecondPlayer)
-				fmt.Printf("Wait for Second:%p, Second found = %p, Go! \n", s, state.SecondPlayer)
 			} else {
-				fmt.Printf("Wait for Second:%p, Waiting = %p \n", s, state.SecondPlayer)
 			}
 		}).TryUse(ctx)
 		if switcher {
@@ -114,7 +108,6 @@ func (s *StateMachine3) WaitForSecond(ctx smachine.ExecutionContext) smachine.St
 			mySharedAccessReport := myGameSharedStateAccessor.Prepare(func(state *GameSharedState) {
 				state.BallOwner = s // I will be owner
 				state.cnt = 0
-				fmt.Printf("Lets Game:%d, Owner: \n", state.cnt, s)
 			}).TryUse(ctx)
 			// Go to Game
 			return smachine.RepeatOrJumpElse(ctx, mySharedAccessReport, s.Game, s.Wrong)
@@ -123,7 +116,7 @@ func (s *StateMachine3) WaitForSecond(ctx smachine.ExecutionContext) smachine.St
 			return smachine.RepeatOrJumpElse(ctx, mySharedAccessReport, s.WaitForSecond, s.Wrong)
 		}
 	} else {
-		fmt.Printf("WaitForSecond: Something wrong \n")
+		fmt.Printf("Error (WaitForSecond): Something wrong \n")
 		return ctx.Jump(s.Wrong)
 	}
 }
@@ -132,7 +125,6 @@ func (s *StateMachine3) WaitForGame(ctx smachine.ExecutionContext) smachine.Stat
 	// Waiting for Game Object
 	// fmt.Printf("WaitForGame: %s \n", s.waitKey)
 	if v, ok := s.catalogC.TryGet(ctx, longbits.ByteString(s.waitKey)) ; ok {
-		fmt.Printf("WaitForGame: Object detected %v \n", v)
 		return ctx.Jump(s.Game)
 	} else {
 		// repeat
@@ -145,21 +137,16 @@ func (s *StateMachine3) WaitForGame(ctx smachine.ExecutionContext) smachine.Stat
 func (s *StateMachine3) Game(ctx smachine.ExecutionContext) smachine.StateUpdate {
 	fmt.Printf("Game %p \n", s)
 	if v, ok := s.catalogD.TryGet(ctx, longbits.ByteString(s.waitKey)) ; ok {
-		fmt.Printf("HERE 0 : %s", v)
 		endgame := false
 		mySharedAccessReport := v.Prepare(func(state *GameSharedState) {
-			fmt.Printf("Game: %p, HERE 1 \n", s)
 			if s != state.BallOwner {
-				fmt.Printf("Game: %p, Change Owner \n", s)
 				state.BallOwner = s
 			}
 			state.cnt += 1
 			if state.cnt > 2 {
 				endgame = true
-				fmt.Printf("EndGame:%d, \n", s)
 			}
 		}).TryUse(ctx)
-		fmt.Printf("Game: %p, HERE 2 %s \n", s, mySharedAccessReport)
 		if endgame {
 			// Go to GameOver
 			return smachine.RepeatOrJumpElse(ctx, mySharedAccessReport, s.GameOver, s.Wrong)
@@ -174,14 +161,13 @@ func (s *StateMachine3) Game(ctx smachine.ExecutionContext) smachine.StateUpdate
 }
 
 func (s *StateMachine3) GameOver(ctx smachine.ExecutionContext) smachine.StateUpdate {
-	fmt.Printf("GameOver %p \n", s)
 	return ctx.Stop()
 	//return ctx.Jump(s.GameOver)
 	//return ctx.Stop()
 }
 
 func (s *StateMachine3) Wrong(ctx smachine.ExecutionContext) smachine.StateUpdate {
-	fmt.Printf("WRONG!!! %p \n", s)
+	fmt.Printf("ErrState: WRONG!!! %p \n", s)
 	return ctx.WaitAnyUntil(time.Now().Add(time.Second)).ThenJump(s.Wrong)
 	//return ctx.Stop()
 }
